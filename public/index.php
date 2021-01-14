aa16837ccd160f504269aea29c3d00b4e1f6f26e
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-01-14 12:32:46 -0500

    init

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$BANDWIDTH_ACCOUNT_ID = getenv("BANDWIDTH_ACCOUNT_ID");
+$BANDWIDTH_USERNANME = getenv("BANDWIDTH_USERNAME");
+$BANDWIDTH_PASSWORD = getenv("BANDWIDTH_PASSWORD");
+$BANDWIDTH_VOICE_APPLICATION_ID = getenv("BANDWIDTH_VOICE_APPLICATION_ID");
+$BASE_URL = getenv("BASE_URL");
+
+$config = new BandwidthLib\Configuration(
+    array(
+        "voiceBasicAuthUserName" => $BANDWIDTH_USERNANME,
+        "voiceBasicAuthPassword" => $BANDWIDTH_PASSWORD
+    )
+);
+
+// Instantiate Bandwidth Client
+$client = new BandwidthLib\BandwidthClient($config);
+
+// Instantiate App
+$app = AppFactory::create();
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+$voice_client = $client->getVoice()->getClient();
+
+$app->post('/outboundCall', function (Request $request, Response $response) {
